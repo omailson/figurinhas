ec9adedad134a3412071bd0a0b77209999aac01c
Mailson Menezes <mailson@gmail.com>
2014-04-30 00:16:13 -0300

    InputWidget: trigger an event when the user starts/stops typing
    Signed-off-by: Mailson Menezes <mailson@gmail.com>

--- js/ui/iteminputwidget.js
+++ js/ui/iteminputwidget.js
@@ -1,27 +1,36 @@
 var ItemInputWidget = function (item) {
     this._item = item;
     this._typeTimeoutID = 0;
+    this._typingEventsEnabled = false;
+    this._startedTypingListener = null;
+    this._stoppedTypingListener = null;
 };
 
 ItemInputWidget.prototype.enableKeyPressEvent = function (callback) {
     this._item.keypress(callback);
 };
 
-ItemInputWidget.prototype.enableListFilter = function (stickersList) {
-    this._item.change((function () {
-        var filter = this.value();
-        stickersList.find("li:not(:contains(" + filter + "))").slideUp();
-        var containsFilter = stickersList.find("li:contains(" + filter + ")");
-        var filterFn = function () { return $(this).html().indexOf(filter) === 0; };
-        // Contains filter but doesn't starts with it
-        containsFilter.not(filterFn).slideUp();
-        // Starts with filter
-        containsFilter.filter(filterFn).slideDown();
-    }).bind(this)).keyup((function () {
-        if (this._typeTimeoutID !== 0)
-            clearTimeout(this._typeTimeoutID);
-        this._typeTimeoutID = setTimeout(ItemInputWidget.prototype._onTypeTimedout.bind(this), 250);
-    }).bind(this));
+ItemInputWidget.prototype.enableTypingEvents = function (startedListener, stoppedListener) {
+    this._startedTypingListener = startedListener;
+    this._stoppedTypingListener = stoppedListener;
+
+    if (this._typingEventsEnabled)
+        return;
+
+    this._typingEventsEnabled = true;
+
+    this._item
+        .change(ItemInputWidget.prototype._stoppedTyping.bind(this))
+        .keyup((function () {
+            if (this._typeTimeoutID !== 0)
+                clearTimeout(this._typeTimeoutID);
+            else
+                this._startedTyping();
+
+            this._typeTimeoutID = setTimeout(
+                ItemInputWidget.prototype._onTypeTimedout.bind(this),
+                250);
+        }).bind(this));
 };
 
 ItemInputWidget.prototype.value = function () {
@@ -42,4 +51,14 @@ ItemInputWidget.prototype.setFocus = function (focus) {
 ItemInputWidget.prototype._onTypeTimedout = function () {
     this._typeTimeoutID = 0;
     this._item.change();
+};
+
+ItemInputWidget.prototype._startedTyping = function () {
+    if (this._startedTypingListener)
+        this._startedTypingListener();
+};
+
+ItemInputWidget.prototype._stoppedTyping = function () {
+    if (this._stoppedTypingListener)
+        this._stoppedTypingListener();
 };
