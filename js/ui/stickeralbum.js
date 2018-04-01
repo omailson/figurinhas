aa773a314b23a15de19f376cef634d7d938cf06f
Mailson Menezes <mailson@gmail.com>
2018-04-01 10:40:42 -0300

    Update for 2018 stickers

--- js/ui/stickeralbum.js
+++ js/ui/stickeralbum.js
@@ -2,27 +2,19 @@ var StickerAlbum = function () {
 };
 
 StickerAlbum.prototype.count = function () {
-    // 640 regular stickers + 9 advertising
-    return 649;
+    // 00 + 1 .. 681
+    return 682;
 };
 
 StickerAlbum.prototype.list = function () {
     var list = [];
-    for (var i = 0; i < 640; i++) {
+
+    list.push("00");
+    for (var i = 1; i <= 681; i++) {
         // apparently this is fast than i.toString();
         // see: http://stackoverflow.com/a/5765401
         list.push(i + "");
     }
-
-    list.push("L1");
-    list.push("L2");
-    list.push("L3");
-    list.push("L4");
-    list.push("J1");
-    list.push("J2");
-    list.push("J3");
-    list.push("J4");
-    list.push("W1");
 
     return list;
 };
